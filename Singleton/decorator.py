ac4e778a1166df2046d939de5598dfcc292f2e7b
Siddhartha Purwar <siddpurwar@gmail.com>
2025-04-22 23:17:52 +0530

    FIX: Singleton

--- Singleton/decorator.py
+++ Singleton/decorator.py
@@ -1,15 +1,19 @@
 def singleton(cls):
     instances = {}
+
     def get_instance(*args, **kargs):
         if cls not in instances:
             instances[cls] = cls(*args, **kargs)
         return instances[cls]
+
     return get_instance
+
 
 @singleton
 class Singleton:
     def __init__(self):
-    print("Singleton instance created")
+        print("Singleton instance created")
+
 
 # Testing the Singleton pattern
 obj1 = Singleton()
